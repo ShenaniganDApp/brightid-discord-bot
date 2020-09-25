d4eca2ab6239fbf751f33eb71e335eedfe2fce27
Adam Stallard <adam.stallard@gmail.com>
2020-09-25 13:01:31 -0700

    Make deep link clickable

--- src/endpoints.js
+++ src/endpoints.js
@@ -7,7 +7,7 @@ const BRIGHT_ID_ENDPOINT_V5 = 'https://app.brightid.org/node/v5'
 const BRIGHTID_VERIFICATION_ENDPOINT = `${BRIGHT_ID_ENDPOINT_V5}/verifications`
 const BRIGHTID_SUBSCRIPTION_ENDPOINT = `${BRIGHT_ID_ENDPOINT_V5}/operations`
 const BRIGHTID_LINK_VERIFICATION_ENDPOINT = "https://app.brightid.org/link-verification/"
-const BRIGHT_ID_APP_DEEPLINK = `brightid://link-verification/${NODE_URL}/${CONTEXT_ID}`
+const BRIGHT_ID_APP_DEEPLINK = `https://app.brightid.org/link-verification/${NODE_URL}/${CONTEXT_ID}`
 const 
 module.exports = {
   NODE_URL,
