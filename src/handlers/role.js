13f00c0500eec4855d50711747a03a5113a1c9a3
YoungKidWarrior <ginelliunit@gmail.com>
2021-02-04 18:24:45 -0800

    add role command to gist data

--- src/handlers/role.js
+++ src/handlers/role.js
@@ -1,24 +1,18 @@
 const fs = require('fs')
 const { error } = require('../error-utils')
+const { readGist, updateGist } = require('../updateOrReadGist')
 module.exports = async function role(member, client, message) {
   if (member.hasPermission('ADMINISTRATOR')) {
     const role = message.content.split(/(?<=^\S+)\s/)[1]
     if (role) {
-      const fileData = JSON.parse(fs.readFileSync('./src/guildData.json'))
-      const previousRole = fileData[member.guild.id].role
+      const guilds = await readGist()
+      const previousRole = guilds[message.guild.id].role
       const guildRole = message.guild.roles.cache.find(
         r => r.name === previousRole,
       )
       try {
         await guildRole.edit({ name: role })
-        fileData[member.guild.id] = {
-          ...fileData[member.guild.id],
-          role,
-        }
-        fs.writeFileSync(
-          './src/guildData.json',
-          JSON.stringify(fileData, null, 2),
-        )
+        await updateGist(message.guild.id, { role })
         message.reply(`Succesfully update verified role to ${role}`)
       } catch (err) {
         console.log('err: ', err)
