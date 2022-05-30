be1074c20df4bf1f8475cb6526836f65c70bc596
VictorGinelli <ginelliunit@gmail.com>
2022-05-29 20:19:30 -0700

    Update Help Command to new commands

--- apps/bot/src/commands/Commands_Help.mjs
+++ apps/bot/src/commands/Commands_Help.mjs
@@ -2,7 +2,7 @@
 
 import * as Builders from "@discordjs/builders";
 
-var helpMessage = "```\n__**Available BrightId commands:**__\n- `/verify` → Sends a BrightID QR code for users to connect with their BrightId\n- `/me` → After scanning the qr code, add yourself to the list of verified users\n- `/guilds` → View a list of discord servers that use this bot. Lots of cool servers use BrightId for token airdrops 😉\n__**admin only**__\n- `/role` → Use this command to change the name of the \"Verified\" role\n- `/invite` → Use this command to add an invite for this discord to the guilds\n```";
+var helpMessage = "```\n__**Available BrightId Unique Bot commands:**__\n\n- `/verify` → Sends a BrightID QR code for users to connect with their BrightId\n\n- `/guilds` → View a list of discord servers that use this bot. Lots of cool servers use BrightId for token airdrops 😉\n\n\nServer Admin only:\n- `/role` → Use this command to change the name of the \"Verified\" role\n\n- `/invite` → Use this command to add an invite for this discord to the guilds\n\n```";
 
 var data = new Builders.SlashCommandBuilder().setName("help").setDescription("Explain the BrightId bot commands");
 
