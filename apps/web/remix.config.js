41afd50dca5ad0d72f41c2cc52027ba9ab42a089
VictorGinelli <ginelliunit@gmail.com>
2022-06-22 01:57:09 -0700

    adjust to deploy remix to vercel

--- apps/web/remix.config.js
+++ apps/web/remix.config.js
@@ -4,6 +4,8 @@ const { registerRoutes } = require('rescript-remix/registerRoutes')
  * @type {import('@remix-run/dev/config').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: 'vercel',
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   appDirectory: 'app',
   assetsBuildDirectory: 'public/build',
   publicPath: '/build/',
@@ -11,7 +13,7 @@ module.exports = {
   devServerPort: 8002,
   ignoredRouteFiles: ['.*', '*.res'],
   transpileModules: ['rescript', 'rescript-webapi'],
-  serverDependenciesToBundle: ['@rainbow-me/rainbowkit'],
+  serverDependenciesToBundle: ['@rainbow-me/rainbowkit, react'],
   routes(defineRoutes) {
     return defineRoutes(route => {
       registerRoutes(route)
